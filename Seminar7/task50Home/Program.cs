83a9c837cb15826d9eef4fa8cc27babd9d885faa
DevG00Ds <semenov.dmitriy.of@gmail.com>
2023-03-04 21:11:50 +0300

    finish task50home

--- Seminar7/task50Home/Program.cs
+++ Seminar7/task50Home/Program.cs
@@ -8,4 +8,51 @@
 
 
 
+int ReadNumber(string messageToUser)
+{
+    Console.Write(messageToUser);
+    return Convert.ToInt32(Console.ReadLine());
+}
 
+int[,] matrix = new int[10, 10];
+
+int[,] GetRandomMatrix(int[,] array)
+{
+
+    for (int i = 0; i < matrix.GetLength(0); i++)
+    {
+        for (int j = 0; j < matrix.GetLength(1); j++)
+        {
+            matrix[i, j] = new Random().Next(-100, 100) / 10;
+
+        }
+    }
+    return matrix;
+}
+
+void PrintMatrix(int[,] matrix)
+{
+    for (int i = 0; i < matrix.GetLength(0); i++)
+    {
+        for (int j = 0; j < matrix.GetLength(1); j++)
+        {
+            Console.Write(matrix[i, j] + " ");
+        }
+        Console.WriteLine();
+    }
+}
+
+int m = ReadNumber(" Введите номер на строке : ");
+int n = ReadNumber(" Введите намер на столбце : ");
+
+GetRandomMatrix(matrix);
+PrintMatrix(matrix);
+
+if (m > matrix.GetLength(0) || n > matrix.GetLength(1))
+{
+    Console.WriteLine("Такого номера нет");
+}
+else
+{
+    Console.WriteLine($"Значение номера {m} строке и {n} в столбце равен {matrix[m - 1, n - 1]}");
+}
