597ff0815006599a361786ddec67c2b8222c5032
DevG00Ds <semenov.dmitriy.of@gmail.com>
2023-03-04 10:36:16 +0300

    finish task49

---
+++ Seminar7/task49/Program.cs
@@ -0,0 +1,72 @@
+﻿// Задача 49: Задайте двумерный массив. Найдите элементы, у которых оба индекса чётные, и замените эти элементы на их квадраты.
+// ​ (в примере подсчет индексов начинается с 1 , как в математике)
+// Например, изначально массив
+// выглядел вот так:
+// 1 4 7 2
+// 5 9 2 3
+// 8 4 2 4
+// ​
+// Новый массив будет выглядеть
+// вот так:
+// 1 4 7 2
+// 5 81 2 9
+// 8 4 2 4
+
+
+int ReadNumber(string messageToUser)
+{
+    Console.Write(messageToUser);
+    return Convert.ToInt32(Console.ReadLine());
+}
+
+int[,] GetRandomMatrix(int rows, int columns, int leftRange = 0, int RightRange = 10)
+{
+    int[,] matrix = new int[rows, columns];
+
+    for (int i = 0; i < matrix.GetLength(0); i++)
+    {
+        for (int j = 0; j < matrix.GetLength(1); j++)
+        {
+            matrix[i, j] = Random.Shared.Next(leftRange, RightRange + 1);
+
+        }
+    }
+    return matrix;
+}
+
+int m = ReadNumber(" Введите количество строк : ");
+int n = ReadNumber(" Введите количество столбцов : ");
+
+int[,] myMatrix = GetRandomMatrix(m, n);
+
+void PrintMatrix(int[,] matrix)
+{
+    for (int i = 0; i < matrix.GetLength(0); i++)
+    {
+        for (int j = 0; j < matrix.GetLength(1); j++)
+        {
+            Console.Write(matrix[i, j] + " ");
+        }
+        Console.WriteLine();
+    }
+}
+
+void matrixSqure(int[,] matrix)
+{
+    for (int i = 0; i < matrix.GetLength(0); i++)
+    {
+        for (int j = 0; j < matrix.GetLength(1); j++)
+        {
+            if (i % 2 != 0 && j % 2 != 0)
+            {
+                matrix[i, j] = matrix[i, j] * matrix[i, j];
+            }
+        }
+    }
+}
+
+PrintMatrix(myMatrix);
+
+matrixSqure(myMatrix);
+Console.WriteLine();
+PrintMatrix(myMatrix);
