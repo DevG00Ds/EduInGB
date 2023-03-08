2511004d77105f9ca2feee3d3a91abeced2a61b1
DevG00Ds <semenov.dmitriy.of@gmail.com>
2023-03-08 14:56:44 +0300

    finish task56home

---
+++ Seminar8/task56Home/Program.cs
@@ -0,0 +1,77 @@
+﻿// Задача 56: Задайте прямоугольный двумерный массив. Напишите программу,
+// которая будет находить строку с наименьшей суммой элементов.
+
+// Например, задан массив:
+// 1 4 7 2
+// 5 9 2 3
+// 8 4 2 4
+// 5 2 6 7
+// Программа считает сумму элементов в каждой строке и выдаёт номер строки с наименьшей суммой элементов: 1 строка
+
+
+Console.Clear();
+int ReadNumber(string MessageToUser)
+{
+    Console.WriteLine(MessageToUser);
+    int value = Convert.ToInt32(Console.ReadLine());
+    return value;
+}
+
+int[,] GetRandomMatrix(int rows, int columns, int leftborder = 0, int rightborder = 9)
+{
+    int[,] matrix = new int[rows, columns];
+
+    for (int i = 0; i < matrix.GetLength(0); i++)
+    {
+        for (int j = 0; j < matrix.GetLength(1); j++)
+        {
+            matrix[i, j] = Random.Shared.Next(leftborder, rightborder + 1);
+
+        }
+
+    }
+    return matrix;
+}
+
+void PrintMatrix(int[,] matrix)
+{
+    for (int i = 0; i < matrix.GetLength(0); i++)
+    {
+        for (int j = 0; j < matrix.GetLength(1); j++)
+        {
+            Console.Write(matrix[i, j] + " ");
+        }
+        Console.WriteLine();
+    }
+}
+
+int m = ReadNumber("Строки");
+int n = ReadNumber("Столбцы");
+int[,] myMatrix = GetRandomMatrix(m, n);
+PrintMatrix(myMatrix);
+
+int SearchSumRow(int[,] matrix, int i)
+{
+    int sumLine = matrix[i, 0];
+    for (int j = 1; j < matrix.GetLength(1); j++)
+    {
+        sumLine += matrix[i, j];
+    }
+    return sumLine;
+}
+
+int minSumRow = 0;
+int sumRow = SearchSumRow(myMatrix, 0);
+
+for (int i = 1; i < myMatrix.GetLength(0); i++)
+{
+  int tempSumLine = SearchSumRow(myMatrix, i);
+  if (sumRow > tempSumLine)
+  {
+    sumRow = tempSumLine;
+    minSumRow = i;
+  }
+}
+
+Console.WriteLine($"\n{minSumRow+1} - строкa с наименьшей суммой ({sumRow}) элементов ");
+Console.WriteLine();
