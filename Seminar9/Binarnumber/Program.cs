b64f87815414d961803e237907513a97a269ac72
DevG00Ds <semenov.dmitriy.of@gmail.com>
2023-03-10 22:35:08 +0300

    finish task67/69

---
+++ Seminar9/Binarnumber/Program.cs
@@ -0,0 +1,13 @@
+﻿
+
+
+
+int number =13; 
+PrintBinaryView(number);
+
+void PrintBinaryView( int n)
+{
+    if ( n==0) return;
+    PrintBinaryView(n/2);
+    Console.Write(n%2);
+}
---
+++ Seminar9/task67/Program.cs
@@ -0,0 +1,22 @@
+﻿// Задача 67: Напишите программу, которая будет принимать на вход число и возвращать сумму его цифр.
+// 453 -> 12
+// 45 -> 9
+
+
+Console.Clear();
+
+int number = 453;
+
+
+int SummaDigits(int n)
+{
+    int sum = 0;
+    if (n == 0) return 0;
+    else
+    {
+        sum += n % 10+ SummaDigits(n/10);
+    }
+    return sum;
+}
+
+Console.WriteLine(SummaDigits(number));
